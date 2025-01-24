2656f56698ef6265b1902a7244fc99ad5f9dee60
Leon <mail@leonkuehn.de>
2025-01-24 12:55:16 +0100

    Layout und code

--- app/src/main/java/com/example/a3wins/MainActivity.java
+++ app/src/main/java/com/example/a3wins/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a3wins;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<ImageView> imageViewList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        int[] imageViewIds = {
+                R.id.imageView6,
+                R.id.imageView7,
+                R.id.imageView8,
+                R.id.imageView9,
+                R.id.imageView10,
+                R.id.imageView11,
+                R.id.imageView12,
+                R.id.imageView13,
+                R.id.imageView14
+        };
+
+
+        Log.i("info",String.valueOf(imageViewIds[1]));
+
+        for (int id : imageViewIds) {
+            ImageView imageView = findViewById(id);
+            imageViewList.add(imageView);
+        }
+
+        imageViewList.get(1).setImageResource(R.drawable.red);
+
+
+
     }
 }
